a94fab1e9881413d302c310302fa1524d777c533
annbanane <89851341+annbanane@users.noreply.github.com>
2021-09-19 16:57:23 -0400

    création d'une base de donnée busDB et fonction préliminaire qui permte l'ajout d'un bus et ses attributs à busDB

---
+++ main.js
@@ -0,0 +1,19 @@
+//création d'une fonction qui permet l'ajout d'une bus dans la base de donnée busDB
+
+
+
+const busDB = [];
+const addBus = function(id, license, make, model, seatCount, standingCount, doorCount, accessCount) {
+    const bus = {
+        id : id,
+        license : license,
+        make : make,
+        model : model,
+        seatCount : seatCount,
+        standingCount : standingCount,
+        doorCount : doorCount,
+        accessCount : accessCount
+    }; 
+    busDB.push(bus);
+    return bus;
+};
