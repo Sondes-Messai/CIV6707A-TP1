351475e0554b9e78d236d3a1ec9a2a1248e71cfa
annbanane <89851341+annbanane@users.noreply.github.com>
2021-09-20 23:13:35 -0400

    ajout structure code interface pour usager
    ajout d'un début de code et de la structure du code pour l'interface de l'utilisateur, incomplet pour supprimer et rechercher closes#3

--- main.js
+++ main.js
@@ -1,7 +1,70 @@
+//Interface utilisateur pour ajouter un autobus, supprimer un autobus, faire une recherche
+const inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+      name: 'actionChoice',
+      type: 'list',
+      message: 'Que voulez-vous faire? (Utilisez les touches flèches et la touche «retour» pour sélectionner):',
+      choices: ['Ajouter un autobus', 'Supprimer un autobus', 'Faire une recherche'],
+    }]) //doit changer les fonction flèches pour des fonctions.
+        // fonctionne jusqu'à ici
+   .then(({actionChoice})=> {
+       switch(actionChoice){
+           case 'Ajouter un autobus':
+               inquirer.prompt([
+                   {
+                    name: 'id',
+                    type: 'number',
+                    message: 'Quel est l\'identifiant de l\'autobus (sans espaces ni charactères spéciaux)?'
+                   },
+                   {
+                    name: 'license',
+                    type: 'input',
+                    message: 'Quel est le numéro de plaque d\'immatriculation de l\'autobus (sans espaces ni charactères spéciaux)?'
+                   },
+                   {
+                    name: 'make',
+                    type: 'input',
+                    message: 'Quel est le fabriquant de l\'autobus?'
+                   },
+                   {
+                    name: 'model',
+                    type: 'input',
+                    message: 'Quel est le modèl de l\'autobus?'
+                   },
+                   {
+                    name: 'seatCount',
+                    type: 'nomber',
+                    message: 'Combien y a-t-il de places assises?'
+                   },
+                   {
+                    name: 'modelstandingCapacity',
+                    type: 'nombre',
+                    message: 'Combien y a-t-il de places debout?'
+                   },
+                   {
+                    name: 'doorCount',
+                    type: 'nombre',
+                    message: 'Combien y a-t-il de portes?'
+                   },
+                   {
+                    name: 'accessCount',
+                    type: 'nombre',
+                    message: 'Combien y a-t-il d\'accès?'
+                   },
+                ])
+                break;
+            case 'Supprimer un autobus' :
+                console.log('code pour supprimer');
+                break;
+            default : 
+                console.log('code pour recher à ajouter')
+       }
+   }) 
+
+
 //création d'une fonction qui permet l'ajout d'un bus dans la base de donnée busDB
-
-
-
 const busDB = [];
 const addBus = function(id, license, make, model, seatCount, standingCount, doorCount, accessCount) {
     const bus = {
